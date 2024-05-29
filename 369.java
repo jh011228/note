3ccd18010b6db7055eb7840eb6cd4bd30d5114d9
jh011228 <jh01122844@gmail.com>
2024-05-29 13:33:29 +0900

    Create 369.java

---
+++ 369.java
@@ -0,0 +1,28 @@
+```java
+public class main {
+	public static void main(String[] args) {
+		for (int i=1;i<100;i++){
+			if(i/10==3||i%10==3) {
+				System.out.println("짝");
+			}
+    for (int i=1;i<100;i++){
+			if(i/10==6||i%10==6) {
+				System.out.println("짝");
+			}
+		for (int i=1;i<100;i++){
+			if(i/10==9||i%10==9) {
+				System.out.println("짝");
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+```
